3941e4261c2cdf814042ff50b4b31657b9775312
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-07-16 02:10:30 +0400

    #131; Swepeable Bookings

---
+++ app/bookings/booking.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Booking } from './booking.model';
+
+@Injectable({providedIn: 'root'})
+export class BookingService {
+    private _bookings: Booking[] = [
+        new Booking( 'xyz','p1','abc','Manhattan Mansion', 2),
+    ];
+    get bookings() {
+        return [...this._bookings];
+    }
+}
