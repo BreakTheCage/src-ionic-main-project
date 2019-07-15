266e91180485b060c0bd9c3871d7198b29c46cbd
BreakTheCage <sarbaztanha707@outlook.com>
2019-07-15 14:25:01 +0400

    #Establish src; 112;

---
+++ app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-offer-bookings',
+  templateUrl: './offer-bookings.page.html',
+  styleUrls: ['./offer-bookings.page.scss'],
+})
+export class OfferBookingsPage implements OnInit {
+  place: Place;
+  constructor(private route: ActivatedRoute, private navCtrl: NavController ,private placesService: PlacesService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      console.log('ParamMap: ', paramMap);
+      if(!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      console.log('Starting extract place with placeId of: ', paramMap.get('placeId'));
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      console.log('Selected Place: ', this.place);
+
+    });
+  }
+
+}
