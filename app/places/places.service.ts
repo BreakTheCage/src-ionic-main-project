ca30ff136fd991d6782467271b42868fd56043b5
SaeedYaghuti <sarbaztanha707@gmail.com>
2019-07-18 00:54:41 +0400

    #172: On Going...

--- app/places/places.service.ts
+++ app/places/places.service.ts
@@ -11,63 +11,81 @@ export class PlacesService {
       'Manhatan Mansion',
       'In the heart of New York City',
       'https://a.1stdibscdn.com/archivesE/upload/a_241/a_26038411520285183192/Architectural_024_mae_2018_master.jpg?width=768',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L Amoure Tojoure',
       'A romantic place in Paris!',
       'https://louisfeedsdc.com/wp-content/uploads/french-style-home_383033.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'Foggy Palace',
       'Not your average city trip!',
       'https://i0.wp.com/www.photographyandtravel.com/wp-content/uploads/2017/05/B2.-Pena-Palace-043-Edit.jpg?resize=1090%2C807',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p1',
       'Manhatan Mansion',
       'In the heart of New York City',
       'https://a.1stdibscdn.com/archivesE/upload/a_241/a_26038411520285183192/Architectural_024_mae_2018_master.jpg?width=768',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L Amoure Tojoure',
       'A romantic place in Paris!',
       'https://louisfeedsdc.com/wp-content/uploads/french-style-home_383033.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'Foggy Palace',
       'Not your average city trip!',
       'https://i0.wp.com/www.photographyandtravel.com/wp-content/uploads/2017/05/B2.-Pena-Palace-043-Edit.jpg?resize=1090%2C807',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p1',
       'Manhatan Mansion',
       'In the heart of New York City',
       'https://a.1stdibscdn.com/archivesE/upload/a_241/a_26038411520285183192/Architectural_024_mae_2018_master.jpg?width=768',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L Amoure Tojoure',
       'A romantic place in Paris!',
       'https://louisfeedsdc.com/wp-content/uploads/french-style-home_383033.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'Foggy Palace',
       'Not your average city trip!',
       'https://i0.wp.com/www.photographyandtravel.com/wp-content/uploads/2017/05/B2.-Pena-Palace-043-Edit.jpg?resize=1090%2C807',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
   get places() {
